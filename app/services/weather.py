3bc3e9aa1b0b1ae1f9a07cb2185edbd8278aa3ab
allangoncha <allan.goncharenco@hotmail.com>
2024-06-02 17:08:00 -0300

    Inclusao de variaveis de ambiente do mongodb e insercao do response na collection.

--- app/services/weather.py
+++ app/services/weather.py
@@ -1,15 +1,31 @@
 from dotenv import load_dotenv
-import pymongo, os, requests, json
+from pymongo import MongoClient
+import os, requests, json
 
 load_dotenv()
 URL = os.getenv("url")
 API_KEY = os.getenv("api_key")
+MONGODB_HOST = os.getenv("mongodb_host")
+MONGODB_PORT = os.getenv("mongodb_port")
+MONGODB_USER = os.getenv("mongodb_user")
+MONGODB_PASS = os.getenv("mongodb_pass")
 
 class WeatherService:
     def __init__(self):
-        pass
+
+        try:
+            #Init MongoClient
+            self.client = MongoClient()
+            self.client = MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_HOST}:{MONGODB_PORT}/")
+            
+        except Exception as e:
+            print(e)
 
     async def process_request(self, request):
+
+        db = self.client['weather']
+        collection = db['weather_datas']
+
         params = {
             'lat': request.lat,
             'lon': request.lon,
@@ -21,4 +37,15 @@ class WeatherService:
 
         response = requests.request("GET", URL, params=params)
 
-        return json.loads(response.text)
+        if response.status_code == 200:
+            res = json.loads(response.text)
+            inserted_id = collection.insert_one(res).inserted_id
+            res['_id'] = str(inserted_id)
+            return res
+        
+        else:
+            return {"Error": {
+                "msg": "Erro de retorno openweatherapi",
+                "status": response.status_code,
+            }}
+        
